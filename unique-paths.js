d06535e2f1968d54555edc91a19d85d0e16cb177
Sean <seanmoore1976@gmail.com>
2023-04-07 10:12:26 -0700

    uniquePaths refactoring
    Refactoring for uniquePaths algorithm in an effort to clarify code.

--- unique-paths.js
+++ unique-paths.js
@@ -1,26 +1,14 @@
 const uniquePaths = (m, n) => {
-  // Create a 2D grid to store the number of paths for each cell
-  const grid = Array.from({ length: m }, () =>
-    Array.from({ length: n }, () => 0),
-  )
+  // Create dp array
+  const dp = new Array(n + 1).fill(1)
 
-  // Set the number of paths for the first row and column to 1
-  for (let i = 0; i < m; i++) {
-    grid[i][0] = 1
-  }
-  for (let j = 0; j < n; j++) {
-    grid[0][j] = 1
-  }
-
-  // Calculate the number of paths for each cell in the grid
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+  // Populate dp array
+  for (let row = m - 1; row > 0; row--) {
+    for (let col = n - 1; col > 0; col--) {
+      dp[col] = dp[col] + dp[col + 1]
     }
   }
-
-  // Return the number of paths for the bottom-right cell
-  return grid[m - 1][n - 1]
+  return dp[1]
 }
 
 console.log(`uniquePaths(3,7): ${uniquePaths(3, 7)}`) // 28
