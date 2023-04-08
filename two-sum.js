037130b23baa3a84c6eb5302b5532a29041c003b
Sean <seanmoore1976@gmail.com>
2023-04-07 19:08:10 -0700

    Adding LeetCode problem statements
    Adding LeetCode problem statements and time complexities.

--- two-sum.js
+++ two-sum.js
@@ -1,25 +1,40 @@
 /*
-function twoSum(nums, target) {
-  const seen = {} // create an object to store seen values
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i] // calculate the complement
+1. Two Sum
+https://leetcode.com/problems/two-sum/
 
-    console.log(`complement: ${complement}`)
-    console.log(`seen[complement]: ${seen[complement]}`)
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-    if (seen[complement] !== undefined) {
-      // if the complement is in the object, return the indices
-      return [seen[complement], i]
-    }
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-    seen[nums[i]] = i // store the current value in the object
-  }
+You can return the answer in any order.
 
-  return null // if no solution found, return null
-}
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
 */
 
+// The time complexity of the given code is O(n), where n is the length of the input numbers array.
 function twoSumHT(numbers, target) {
   const ht = {}
 
@@ -33,6 +48,7 @@ function twoSumHT(numbers, target) {
   }
 }
 
+// The time complexity of the given code is O(n log n), where n is the length of the input nums array.
 function twoSum(nums, target) {
   const sortedNums = nums.slice().sort((a, b) => a - b)
   let left = 0
