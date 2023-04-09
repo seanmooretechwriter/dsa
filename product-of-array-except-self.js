5f9f1d6da542e8f009c05b9f1710a9f8be61833f
Sean <seanmoore1976@gmail.com>
2023-04-09 16:13:57 -0700

    Product of Array Except Self refactoring
    Add problem statement, URL and FP variant to Product of Array Except Self.

--- product-of-array-except-self.js
+++ product-of-array-except-self.js
@@ -1,3 +1,25 @@
+/*
+
+238. Product of Array Except Self
+https://leetcode.com/problems/product-of-array-except-self/
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+*/
+
 const productExceptSelfSimple = (nums) => {
   const output = []
   let product = 1
@@ -19,7 +41,7 @@ const productExceptSelfSimple = (nums) => {
   return output
 }
 
-function productExceptSelf(nums) {
+const productExceptSelf = (nums) => {
   const n = nums.length
   const output = new Array(n).fill(1)
   let leftProduct = 1
@@ -37,3 +59,19 @@ function productExceptSelf(nums) {
 
 const a = [1, 2, 3, 4]
 console.log(`productExceptSelf(${a}): ${productExceptSelf(a)}`)
+
+const productExceptSelfFP = (nums) => {
+  const n = nums.length
+  const leftProducts = nums.reduce(
+    (acc, curr) => [...acc, acc[acc.length - 1] * curr],
+    [1],
+  )
+  const rightProducts = nums.reduceRight(
+    (acc, curr) => [curr * acc[0], ...acc],
+    [1],
+  )
+  const output = leftProducts.map(
+    (left, index) => left * rightProducts[index + 1],
+  )
+  return output
+}
