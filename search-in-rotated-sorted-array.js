9c94ceb78e926e25396934b983d0b59fd2da5d49
Sean <seanmoore1976@gmail.com>
2023-04-09 17:24:08 -0700

    Search in Rotated Sorted Array refactoring
    Add problems statement, URL and FP code variant.

--- search-in-rotated-sorted-array.js
+++ search-in-rotated-sorted-array.js
@@ -1,3 +1,40 @@
+/*
+
+33. Search in Rotated Sorted Array
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+
+*/
+
+// The time complexity of the search function is O(log n), where n is the length of the nums array.
 const search = (nums, target) => {
   let left = 0
   let right = nums.length - 1
@@ -45,48 +82,36 @@ const search = (nums, target) => {
   return -1
 }
 
-//const a = [4, 5, 6, 7, 0, 1, 2]
-//console.log(`search(a, 3): ${search(a, 3)}`)
-
-//const b = [5, 6, 7, 0, 1, 2, 3]
-//const b = [13, 12, 11, 10, 9, 8, 7, 5, 0, 1, 2, 3]
 const b = [13, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 const t = 2
 console.log(`search(${b}, ${t}): ${search(b, 2)}`)
 
-//[4,5,6,7,0,1,2], target = 3
+const searchFP = (nums, target) => {
+  const binarySearch = (left, right) => {
+    if (left > right) {
+      return -1
+    }
 
-/*
-iterator: 1, left: nums[0]: 13, mid: nums[6]: 5, right: nums[12]: 11
-iterator: 2, left: nums[0]: 13, mid: nums[2]: 1, right: nums[5]: 4
-iterator: 3, left: nums[3]: 2, mid: nums[4]: 3, right: nums[5]: 4
-iterator: 4, left: nums[3]: 2, mid: nums[3]: 2, right: nums[3]: 2
-search(13,12,1,2,3,4,5,6,7,8,9,10,11, 2): 3
-*/
+    const mid = Math.floor((left + right) / 2)
 
-/*
-iterator: 1, left: nums[0]: 13, mid: nums[6]: 5, right: nums[12]: 11
-isNumsLeftLessThanEqualNumsMid: false
-isNumsLeftLessThanEqualTarget: false
-isTargetLessThanNumsMid: true
-isNumsMidLessThanTarget: false
-isTargetLessThanEqualNumsRight: true
-iterator: 2, left: nums[0]: 13, mid: nums[2]: 1, right: nums[5]: 4
-isNumsLeftLessThanEqualNumsMid: false
-isNumsLeftLessThanEqualTarget: false
-isTargetLessThanNumsMid: false
-isNumsMidLessThanTarget: true
-isTargetLessThanEqualNumsRight: true
-iterator: 3, left: nums[3]: 2, mid: nums[4]: 3, right: nums[5]: 4
-isNumsLeftLessThanEqualNumsMid: true
-isNumsLeftLessThanEqualTarget: true
-isTargetLessThanNumsMid: true
-isNumsMidLessThanTarget: false
-isTargetLessThanEqualNumsRight: true
-iterator: 4, left: nums[3]: 2, mid: nums[3]: 2, right: nums[3]: 2
-isNumsLeftLessThanEqualNumsMid: true
-isNumsLeftLessThanEqualTarget: true
-isTargetLessThanNumsMid: false
-isNumsMidLessThanTarget: false
-isTargetLessThanEqualNumsRight: true
-*/
+    if (nums[mid] === target) {
+      return mid
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        return binarySearch(left, mid - 1)
+      } else {
+        return binarySearch(mid + 1, right)
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[right]) {
+        return binarySearch(mid + 1, right)
+      } else {
+        return binarySearch(left, mid - 1)
+      }
+    }
+  }
+
+  return binarySearch(0, nums.length - 1)
+}
