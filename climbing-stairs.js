d36016259db319339c77736af8931d4192bd39ad
Sean <seanmoore1976@gmail.com>
2023-04-09 17:59:56 -0700

    Climbing Stairs refactoring
    Add problem statement, URL and FP code variant.

--- climbing-stairs.js
+++ climbing-stairs.js
@@ -1,22 +1,35 @@
-const climbStairsGPT = (n) => {
-  // Create an array to store the number of ways to climb each step
-  const dp = new Array(n + 1).fill(0)
+/*
 
-  // There's only one way to climb 0 and 1 steps
-  dp[0] = 1
-  dp[1] = 1
+70. Climbing Stairs
+https://leetcode.com/problems/climbing-stairs/
 
-  // For each step i, the number of ways to climb it is the sum of the number
-  // of ways to climb the previous step (i-1) and the number of ways to climb
-  // the step before the previous one (i-2)
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2]
-  }
+You are climbing a staircase. It takes n steps to reach the top.
 
-  // Return the number of ways to climb n steps
-  return dp[n]
-}
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+Constraints:
+
+1 <= n <= 45
+
+*/
+
+// The time complexity of the given code is O(n), where n is the input value n.
 const climbStairs = (n) => {
   const steps = [1, 1]
   for (let i = 2; i < n + 1; i++) {
@@ -28,3 +41,10 @@ const climbStairs = (n) => {
 console.log(`climbStairs(2): ${climbStairs(2)}`)
 console.log(`climbStairs(3): ${climbStairs(3)}`)
 console.log(`climbStairs(8): ${climbStairs(8)}`)
+
+const climbStairsFP = (n) => {
+  const steps = Array.from({ length: n + 1 }, (_, i) =>
+    i < 2 ? 1 : steps[i - 1] + steps[i - 2],
+  )
+  return steps[n]
+}
