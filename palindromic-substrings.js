e6925951066db039ee7934a956523d3a9d2ec2ee
Sean <seanmoore1976@gmail.com>
2023-04-18 16:30:54 -0700

    Palindromic Substrings -- add code to repo
    Initial implementation of Palindromic Substrings algorithm.

---
+++ palindromic-substrings.js
@@ -0,0 +1,68 @@
+/*
+
+647. Palindromic Substrings
+https://leetcode.com/problems/palindromic-substrings/
+
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string. 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+
+*/
+
+// The runtime complexity of the countSubstrings function is O(n^2), where n is the length of the input string s.
+
+const countSubstrings = (s) => {
+  const n = s.length
+  const dp = [...Array(n)].map(() => Array(n).fill(false))
+  let count = 0
+  // Base case: single letter substrings
+  for (let i = 0; i < n; i++) {
+    dp[i][i] = true
+    count++
+  }
+
+  // Base case: double letter substrings
+  for (let i = 0; i < n - 1; i++) {
+    dp[i][i + 1] = s[i] === s[i + 1]
+    dp[i][i + 1] && count++
+  }
+
+  // substrings longer than 2 chars
+  for (let len = 3; len <= n; len++) {
+    let start = 0,
+      end = start + len - 1
+
+    while (end < n) {
+      dp[start][end] = dp[start + 1][end - 1] && s[start] === s[end]
+      dp[start][end] && count++
+      start++
+      end++
+    }
+  }
+  return count
+}
+
+const s = 'abc'
+console.log(`countSubstrings:(${s}) ${countSubstrings(s)}`) // 3
+
+const s2 = 'aaa'
+console.log(`countSubstrings:(${s2}) ${countSubstrings(s2)}`) // 6
