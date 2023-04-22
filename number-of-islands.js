97f11161a32a069b36b008f018560605e8d89936
Sean <seanmoore1976@gmail.com>
2023-04-21 19:55:29 -0700

    Number of Islands -- add code to repo
    Initial algorithm implementation for Number of Islands LeetCode problem.

---
+++ number-of-islands.js
@@ -0,0 +1,89 @@
+/*
+
+200. Number of Islands
+https://leetcode.com/problems/number-of-islands/
+
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+
+*/
+
+// The runtime complexity of the numIslands function is O(MN), where M is the number of rows in the grid and N is the number of columns in the grid.
+const numIslands = function (grid) {
+  if (!grid || grid.length === 0) {
+    return 0
+  }
+
+  const dfs = (i, j) => {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      grid[i][j] === '0'
+    ) {
+      return
+    }
+    grid[i][j] = '0'
+    dfs(i - 1, j)
+    dfs(i + 1, j)
+    dfs(i, j - 1)
+    dfs(i, j + 1)
+  }
+
+  let count = 0
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '1') {
+        count++
+        dfs(i, j)
+      }
+    }
+  }
+
+  return count
+}
+
+const grid = [
+  ['1', '1', '1', '1', '0'],
+  ['1', '1', '0', '1', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '0', '0', '0'],
+]
+console.log('numIslands(grid): ', numIslands(grid)) // 1
+
+const grid2 = [
+  ['1', '1', '0', '0', '0'],
+  ['1', '1', '0', '0', '0'],
+  ['0', '0', '1', '0', '0'],
+  ['0', '0', '0', '1', '1'],
+]
+console.log('numIslands(grid2): ', numIslands(grid2)) // 3
