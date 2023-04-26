9943cc9617206f2fa5f8beb51370410b96202f4d
Sean <seanmoore1976@gmail.com>
2023-04-25 17:24:01 -0700

    Top K Frequent Elements -- algorithm edits
    Update algorithm code for additional clarity.

--- top-k-frequent-elements.js
+++ top-k-frequent-elements.js
@@ -28,29 +28,48 @@ Follow up: Your algorithm's time complexity must be better than O(n log n), wher
 
 // The runtime complexity for the topKFrequent function is O(n log k), where n is the length of the input array and k is the number of elements to return.
 function topKFrequent(nums, k) {
-  const freqMap = new Map()
+  // Create a hash map to store the frequency of each number
+  const freqMap = {}
   for (let num of nums) {
-    freqMap.set(num, (freqMap.get(num) || 0) + 1)
+    freqMap[num] = (freqMap[num] || 0) + 1
   }
 
-  const minHeap = new MinHeap()
-  for (let [num, freq] of freqMap) {
-    minHeap.insert({ num, freq })
-    if (minHeap.size() > k) {
-      minHeap.remove()
+  // Create a min heap to store the top k frequent numbers
+  const heap = new MinHeap((a, b) => freqMap[a] - freqMap[b])
+  for (let num in freqMap) {
+    heap.push(num)
+    if (heap.size() > k) {
+      heap.pop()
     }
   }
 
+  // Return the top k frequent numbers
   const result = []
-  while (!minHeap.isEmpty()) {
-    result.unshift(minHeap.remove().num)
+  while (!heap.isEmpty()) {
+    result.unshift(heap.pop())
   }
   return result
 }
 
 class MinHeap {
-  constructor() {
+  constructor(compareFunc) {
+    this.compare = compareFunc
     this.heap = []
+  }
+
+  push(val) {
+    this.heap.push(val)
+    this.bubbleUp(this.heap.length - 1)
+  }
+
+  pop() {
+    const min = this.heap[0]
+    const end = this.heap.pop()
+    if (this.heap.length > 0) {
+      this.heap[0] = end
+      this.bubbleDown(0)
+    }
+    return min
   }
 
   size() {
@@ -61,56 +80,51 @@ class MinHeap {
     return this.heap.length === 0
   }
 
-  insert(value) {
-    this.heap.push(value)
-    this.bubbleUp(this.heap.length - 1)
-  }
-
-  remove() {
-    const minValue = this.heap[0]
-    const lastValue = this.heap.pop()
-    if (!this.isEmpty()) {
-      this.heap[0] = lastValue
-      this.bubbleDown(0)
-    }
-    return minValue
-  }
-
-  bubbleUp(index) {
-    const parentIndex = Math.floor((index - 1) / 2)
-    if (index > 0 && this.heap[index].freq < this.heap[parentIndex].freq) {
-      this.swap(index, parentIndex)
-      this.bubbleUp(parentIndex)
+  bubbleUp(idx) {
+    const element = this.heap[idx]
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2)
+      const parent = this.heap[parentIdx]
+      if (this.compare(element, parent) >= 0) {
+        break
+      }
+      this.heap[idx] = parent
+      this.heap[parentIdx] = element
+      idx = parentIdx
     }
   }
 
-  bubbleDown(index) {
-    const leftChildIndex = 2 * index + 1
-    const rightChildIndex = 2 * index + 2
-    let minIndex = index
-    if (
-      leftChildIndex < this.heap.length &&
-      this.heap[leftChildIndex].freq < this.heap[minIndex].freq
-    ) {
-      minIndex = leftChildIndex
-    }
-    if (
-      rightChildIndex < this.heap.length &&
-      this.heap[rightChildIndex].freq < this.heap[minIndex].freq
-    ) {
-      minIndex = rightChildIndex
-    }
-    if (index !== minIndex) {
-      this.swap(index, minIndex)
-      this.bubbleDown(minIndex)
-    }
-  }
+  bubbleDown(idx) {
+    const element = this.heap[idx]
+    const length = this.heap.length
+    while (true) {
+      const leftChildIdx = idx * 2 + 1
+      const rightChildIdx = idx * 2 + 2
+      let leftChild, rightChild
+      let swap = null
 
-  swap(index1, index2) {
-    ;[this.heap[index1], this.heap[index2]] = [
-      this.heap[index2],
-      this.heap[index1],
-    ]
+      if (leftChildIdx < length) {
+        leftChild = this.heap[leftChildIdx]
+        if (this.compare(leftChild, element) < 0) {
+          swap = leftChildIdx
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.heap[rightChildIdx]
+        if (
+          (swap === null && this.compare(rightChild, element) < 0) ||
+          (swap !== null && this.compare(rightChild, leftChild) < 0)
+        ) {
+          swap = rightChildIdx
+        }
+      }
+      if (swap === null) {
+        break
+      }
+      this.heap[idx] = this.heap[swap]
+      this.heap[swap] = element
+      idx = swap
+    }
   }
 }
 
