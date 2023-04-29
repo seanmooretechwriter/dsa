c77071fcd3fc4f7d27846d8dadcc0f78638b155e
Sean <seanmoore1976@gmail.com>
2023-04-28 18:41:33 -0700

    Continued study session.
    Updates throughout code for refinement of understanding and readability.

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -3,7 +3,8 @@
 217. Contains Duplicate
 https://leetcode.com/problems/contains-duplicate/
 
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
 
 Example 1:
 
--- two-sum.js
+++ two-sum.js
@@ -3,9 +3,11 @@
 1. Two Sum
 https://leetcode.com/problems/two-sum/
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Given an array of integers nums and an integer target, return indices of the two 
+numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You may assume that each input would have exactly one solution, and you may not use 
+the same element twice.
 
 You can return the answer in any order.
 
@@ -14,10 +16,12 @@ Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 Example 2:
 
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
+
 Example 3:
 
 Input: nums = [3,3], target = 6
@@ -47,27 +51,3 @@ function twoSumHT(numbers, target) {
     ht[numbers[i]] = i
   }
 }
-
-// The time complexity of the given code is O(n log n), where n is the length of the input nums array.
-function twoSum(nums, target) {
-  const sortedNums = nums.slice().sort((a, b) => a - b)
-  let left = 0
-  let right = sortedNums.length - 1
-
-  while (left < right) {
-    const sum = sortedNums[left] + sortedNums[right]
-    if (sum === target) {
-      const leftIndex = nums.indexOf(sortedNums[left])
-      const rightIndex = nums.lastIndexOf(sortedNums[right])
-      return [leftIndex, rightIndex]
-    } else if (sum < target) {
-      left++
-    } else {
-      right--
-    }
-  }
-
-  return []
-}
-
-console.log(`twoSum: ${twoSum([4, 8, 3, 1, 12], 7)}`)
